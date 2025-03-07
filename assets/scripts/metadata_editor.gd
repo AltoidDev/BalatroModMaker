710cb8239e0fca2efd6908e382f76252b4e5e65f
Nova <100539243+AltoidDev@users.noreply.github.com>
2025-03-07 10:04:35 -0800

    Started metadata script

--- assets/scripts/metadata_editor.gd
+++ assets/scripts/metadata_editor.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+@onready var line_edit_id: LineEdit = $Control/VBoxContainer/HBoxContainer/LineEditID
+@onready var line_edit_name: LineEdit = $Control/VBoxContainer/HBoxContainer2/LineEditName
+@onready var line_edit_author: LineEdit = $Control/VBoxContainer/HBoxContainer3/LineEditAuthor
+@onready var line_edit_desc: LineEdit = $Control/VBoxContainer/HBoxContainer4/LineEditDesc
+@onready var line_edit_prefix: LineEdit = $Control/VBoxContainer/HBoxContainer5/LineEditPrefix
+@onready var line_edit_prio: LineEdit = $Control/VBoxContainer/HBoxContainer6/LineEditPrio
+@onready var line_edit_version: LineEdit = $Control/VBoxContainer/HBoxContainer7/LineEditVersion
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
